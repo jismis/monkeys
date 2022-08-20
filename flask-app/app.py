6ca68cdfbed5921f4764d2b7945aa3514130f76c
manjistha <manjisthalakhotia@gmail.com>
2022-08-20 15:38:30 -0500

    flask_cors in venv env add
    make sure that venv is enabled

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,11 +1,30 @@
 # app.py
 from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+major = [
+    {
+        "id":1,
+        "name": "Information Technology",
+        "shortname":"IT"
+    },
+    {
+        "id":2,
+        "name": "Computer Network Technology",
+        "shortname":"CNT"
+    },
+]
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/major')
+def major_api():
+    return jsonify(major)
 
 if __name__=="__main__":
     app.run(debug=False)
