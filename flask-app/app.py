6c559d0f21fe8edba1d2f664e1fcb27c6d27fe06
manjistha <manjisthalakhotia@gmail.com>
2022-08-20 15:15:44 -0500

    new yt edits

--- flask-app/app.py
+++ flask-app/app.py
@@ -2,8 +2,12 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route("/")
-def index():
-    return "Hello World!"
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+if __name__=="__main__":
+    app.run(debug=False)
 
 
